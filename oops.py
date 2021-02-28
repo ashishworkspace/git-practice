d3f71d8ca67525bec58da60c88826ce99a2d56cd
Ashish <ashishizofficial@gmail.com>
2021-02-28 17:31:43 +0530

    second local commit

--- oops.py
+++ oops.py
@@ -1,13 +1,24 @@
 class  Visitors:
-	present_age = 2021
-	def __init__(self, name, birth_age):
+	present_year = 2021
+	def __init__(self, name, birth_year):
 		self.name = name
-		self.birth_age = birth_age
+		self.birth_year = birth_year
 	def print_with_hello(self):
 		return "Hello " +  self.name
 	def calculate_age(self):
-		return f"Your age: { Visitors.present_age - self.birth_age }"
+		return f"Your age: { Visitors.present_year - self.birth_year }"
+
 user_name = input('Enter your name: ')
 user1 = Visitors(user_name, 1999)
 print(user1.print_with_hello())
 print(user1.calculate_age())
+
+input_salary = input('Enter the Salary: ')
+class Employee(Visitors):
+	def __init__(self, name, birth_year, salary):
+		super().__init__(name, birth_year)     # I don't want birth_year variable
+		self.salary = salary
+	def print_employee_salary(self):
+		return  f"{self.name} your salary is {self.salary}"
+user2 = Employee(user_name, 2000, input_salary)
+print(user2.print_employee_salary())
